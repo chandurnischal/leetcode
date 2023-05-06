805d6dc40de183d99e3f26121399b70375fcaf23
warlock7867 <chandur.nischal@gmail.com>
2023-05-06 16:26:33 +0530

    Q13

---
+++ romanToInt.go
@@ -0,0 +1,24 @@
+package main
+
+// question 13
+func RomanToInt(s string) int {
+	roman := map[byte]int{
+		'I': 1,
+		'V': 5,
+		'X': 10,
+		'L': 50,
+		'C': 100,
+		'D': 500,
+		'M': 1000,
+	}
+	n := len(s)
+	num := roman[s[n-1]]
+	for i := n - 2; i >= 0; i-- {
+		if roman[s[i]] < roman[s[i+1]] {
+			num -= roman[s[i]]
+		} else {
+			num += roman[s[i]]
+		}
+	}
+	return num
+}
