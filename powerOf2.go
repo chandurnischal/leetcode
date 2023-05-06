d3c8f45e8b4e6899bd6a373d90541f962987c415
warlock7867 <chandur.nischal@gmail.com>
2023-05-06 11:18:49 +0530

    Q231

---
+++ powerOf2.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+// question 231
+func IsPowerOf2(n int) bool {
+	if n < 0 {
+		return false
+	}
+	nBin := fmt.Sprintf("%b", n)
+	sum := 0
+
+	for _, b := range nBin {
+		intB, _ := strconv.Atoi(string(b))
+		sum += intB
+	}
+	return sum == 1
+}
