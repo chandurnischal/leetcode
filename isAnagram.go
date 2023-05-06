b835af3213a9ac34274b06cc596b4d01fd099fa7
warlock7867 <chandur.nischal@gmail.com>
2023-05-06 11:17:09 +0530

    Q242

---
+++ isAnagram.go
@@ -0,0 +1,30 @@
+package main
+
+// question 242
+func IsAnagram(str1, str2 string) bool {
+	if len(str1) != len(str2) {
+		return false
+	}
+
+	freq1, freq2 := make(map[rune]int), make(map[rune]int)
+	chars := []rune{}
+
+	for _, s := range str1 {
+		_, ok := freq1[s]
+		if !ok {
+			chars = append(chars, s)
+		}
+		freq1[s]++
+	}
+	for _, s := range str2 {
+		freq2[s]++
+	}
+
+	for _, char := range chars {
+		if freq1[char] != freq2[char] {
+			return false
+		}
+	}
+
+	return true
+}
