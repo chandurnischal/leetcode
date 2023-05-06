f5fe141ea386eb38253a2e54da83c0ee700ad46f
warlock7867 <chandur.nischal@gmail.com>
2023-05-06 11:16:52 +0530

    Q693

---
+++ alternatingBits.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+// question 693
+func HasAlternatingBits(n int) bool {
+	binN := fmt.Sprintf("%b", n)
+	l := len(binN)
+
+	for i := 0; i < l-1; i++ {
+		if binN[i] == binN[i+1] {
+			return false
+		}
+	}
+	return true
+}
