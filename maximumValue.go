38bb516f45e2317cd8a97302d4013221cfc83435
warlock7867 <chandur.nischal@gmail.com>
2023-05-06 11:18:32 +0530

    Q2496

---
+++ maximumValue.go
@@ -0,0 +1,31 @@
+package main
+
+import "strconv"
+
+func valueOfString(str string) int {
+	sum := ""
+
+	for _, s := range str {
+		_, err := strconv.Atoi(string(s))
+		if err != nil {
+			return len(str)
+		}
+		sum += string(s)
+	}
+	res, _ := strconv.Atoi(sum)
+	return res
+}
+
+// question 2496
+func MaximumValue(strs []string) int {
+	maxVal := valueOfString(strs[0])
+
+	for _, str := range strs {
+		val := valueOfString(str)
+
+		if val > maxVal {
+			maxVal = val
+		}
+	}
+	return maxVal
+}
