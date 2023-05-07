e12b6680182aa31c80092e8ea4127d119bcc5bef
warlock7867 <chandur.nischal@gmail.com>
2023-05-07 21:06:14 +0530

    Q58

---
+++ lengthOfLAstWord.go
@@ -0,0 +1,12 @@
+package main
+
+import "regexp"
+
+// question 58
+func LengthOfLastWord(str string) int {
+	pattern := regexp.MustCompile("[a-zA-Z]+")
+
+	match := pattern.FindAllStringIndex(str, -1)
+	n := len(match) - 1
+	return match[n][1] - match[n][0]
+}
