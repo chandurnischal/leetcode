6f306f21cc74185bb1e44b1882efe106415c817a
warlock7867 <chandur.nischal@gmail.com>
2023-05-07 20:30:20 +0530

    Q14

---
+++ longestPrefix.go
@@ -0,0 +1,18 @@
+package main
+
+// question 14
+func LongestPrefix(strs []string) string {
+	res := ""
+	n := len(strs[0])
+
+	for i := 0; i < n; i++ {
+		for _, s := range strs {
+			if i == len(s) || s[i] != strs[0][i] {
+				return res
+			}
+		}
+		res += string(strs[0][i])
+	}
+	return res
+
+}
