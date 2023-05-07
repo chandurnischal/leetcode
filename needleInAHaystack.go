c468bdffc86ebf9a9926ab21320bfdb8ff7e1831
warlock7867 <chandur.nischal@gmail.com>
2023-05-07 20:44:33 +0530

    Q28

---
+++ needleInAHaystack.go
@@ -0,0 +1,14 @@
+package main
+
+import "regexp"
+
+// question 28
+func NeedleInAHaystack(haystack, needle string) int {
+	re := regexp.MustCompile(needle)
+	match := re.FindSubmatchIndex([]byte(haystack))
+
+	if len(match) == 0 {
+		return -1
+	}
+	return match[0]
+}
