66bb373f3f3ac06b7563692e0eedbd0ebad90e95
warlock7867 <chandur.nischal@gmail.com>
2023-05-08 16:31:57 +0530

    Q1V2

--- main.go
+++ main.go
@@ -1,9 +1,5 @@
 package main
 
-import "fmt"
-
 func main() {
-	nums := []int{2, 5, 5, 11}
-	target := 10
-	fmt.Println(TwoSum(nums, target))
+
 }
--- twoSum.go
+++ twoSum.go
@@ -14,3 +14,18 @@ func TwoSum(nums []int, target int) []int {
 	}
 	return nil
 }
+
+// question 1
+func TwoSumV2(nums []int, target int) []int {
+	prevMap := make(map[int]int)
+
+	for i, num := range nums {
+		diff := target - num
+		if _, ok := prevMap[diff]; !ok {
+			prevMap[num] = i
+		} else {
+			return []int{prevMap[diff], i}
+		}
+	}
+	return nil
+}
