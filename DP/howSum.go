1ad0668713af11457753e96d4bf03ed5a82bc56a
warlock7867 <chandur.nischal@gmail.com>
2023-05-09 19:30:10 +0530

    DP2

---
+++ DP/howSum.go
@@ -0,0 +1,44 @@
+package dp
+
+func HowSumRec(target int, nums []int) []int {
+	if target == 0 {
+		return []int{}
+	}
+	if target < 0 {
+		return nil
+	}
+
+	for _, num := range nums {
+		remainder := target - num
+		res := HowSumRec(remainder, nums)
+		if res != nil {
+			res = append(res, num)
+			return res
+		}
+	}
+	return nil
+}
+
+func HowSumDP(target int, nums []int, memo map[int][]int) []int {
+	if _, ok := memo[target]; ok {
+		return memo[target]
+	}
+	if target == 0 {
+		return []int{}
+	}
+	if target < 0 {
+		return nil
+	}
+
+	for _, num := range nums {
+		remainder := target - num
+		res := HowSumDP(remainder, nums, memo)
+		if res != nil {
+			res = append(res, num)
+			memo[target] = res
+			return memo[target]
+		}
+	}
+	memo[target] = nil
+	return nil
+}
