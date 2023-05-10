521452549f6a2581bb810952df25fb4076104496
warlock7867 <chandur.nischal@gmail.com>
2023-05-10 16:37:38 +0530

    pure leetcode

--- main.go
+++ main.go
@@ -1,5 +1,28 @@
 package main
 
+import (
+	"fmt"
+	"strings"
+)
+
+// func CanConstructRec(target string, wordBank []string) bool {
+// 	if target == "" {
+// 		return true
+// 	}
+
+// 	for _, word := range wordBank {
+// 		index := strings.Index(target, word)
+// 		if index == 0 {
+
+// 		}
+// 	}
+// }
+
 func main() {
+	target := "abcdef"
+	wordBank := []string{"ab", "abc", "cd", "def", "abcd"}
+	index := strings.Index(target, wordBank[0])
+
+	fmt.Println(target[index:])
 
 }
