e59abb04ad6c9ecd510e366cbc92b26c8618cf1c
warlock7867 <chandur.nischal@gmail.com>
2023-05-15 22:01:49 +0530

    Q70

---
+++ climbStairs.go
@@ -0,0 +1,18 @@
+package main
+
+var memo = make(map[int]int)
+
+// question 70
+func ClimbStairs(n int) int {
+	if _, ok := memo[n]; ok {
+		return memo[n]
+	}
+	if n == 1 {
+		return 1
+	}
+	if n == 2 {
+		return 2
+	}
+	memo[n] = ClimbStairs(n-1) + ClimbStairs(n-2)
+	return memo[n]
+}
--- main.go
+++ main.go
@@ -1,10 +1,5 @@
 package main
 
-import "fmt"
-
-
 func main() {
 
-	str := "(("
-	fmt.Println(IsValid(str))
 }
