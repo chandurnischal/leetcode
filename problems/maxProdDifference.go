532474fb99c29e2937cedaffd346523e7f48ef9c
nischal@latlong.ai-at-809660911295 <nischal@latlong.ai>
2023-05-16 12:50:10 +0530

    Q1913

---
+++ problems/maxProdDifference.go
@@ -0,0 +1,11 @@
+package leetcode
+
+import "sort"
+
+// question 1913
+func MaxProductDifference(nums []int) int {
+	n := len(nums)
+	sort.Ints(nums)
+	return (nums[n-1] * nums[n-2]) - (nums[0] * nums[1])
+
+}
